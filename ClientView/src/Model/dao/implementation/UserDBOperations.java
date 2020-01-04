89d23876e2452a757792b0d5d69221840bf36adc
ahmedIbrahemis <ahmed.ibrahem@fci.helwan.edu.eg>
2020-01-04 18:22:22 +0200

    add register

--- ClientView/src/Model/dao/implementation/UserDBOperations.java
+++ ClientView/src/Model/dao/implementation/UserDBOperations.java
@@ -20,27 +20,38 @@ import java.sql.SQLException;
  * @author dell
  */
 public class UserDBOperations {
-    
 
-    public static void login(String  username,String password) {
+    public static void login(String username, String password) {
 
         UserEntity user = new UserEntity();
         user.setUsername(username);
         user.setPassword(password);
         RequestEntity<UserEntity> request = new RequestEntity("UserDBOperations", "login", user);
-        SocketConnection.getInstance().getPrintStreamInstance().println(GsonParser.parseToJson(request));        
+        SocketConnection.getInstance().getPrintStreamInstance().println(GsonParser.parseToJson(request));
     }
 
-    public void loginResponse(Object object){
-        if(object == null){
+    public void loginResponse(Object object) {
+        if (object == null) {
             System.out.println("login failed");
-        }
-        else{   
+        } else {
             System.out.println("login success");
         }
     }
-    public int register(Object user) {
 
-        return -1;
+    public static void register(UserEntity user) {
+
+        RequestEntity<UserEntity> request = new RequestEntity("UserDBOperations", "register", user);
+        SocketConnection.getInstance().getPrintStreamInstance().println(GsonParser.parseToJson(request));
     }
+
+    public void registerResponse(Object object) {
+        if (object != null) {
+
+            System.out.println("registered successfully");
+        } else {
+            System.out.println("error happened in registeration");
+        }
+
+    }
+
 }
---
+++ ClientView/src/clientview/mainXMLController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientview;
+
+/**
+ *
+ * @author AhmedIbrahem
+ */
+public class mainXMLController {
+
+    public void homeBtnAction() {
+
+    }
+
+    public void addFriendBtnAction() {
+
+    }
+
+    public void addTodoAction() {
+
+    }
+
+    public mainXMLController() {
+        String [] name= {"Ebrahim 1","Manar 2","yehia 3"};
+    new MainXMLBase(name);
+    }
+}
