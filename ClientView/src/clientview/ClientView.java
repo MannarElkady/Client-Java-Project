31d535ab7be45d5489ef94751b91e4b46924abdc
ahmedIbrahemis <ahmed.ibrahem@fci.helwan.edu.eg>
2020-01-08 10:47:07 +0200

    Test

--- ClientView/src/clientview/ClientView.java
+++ ClientView/src/clientview/ClientView.java
@@ -58,6 +58,7 @@ public class ClientView extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("test");
         launch(args);
 
     }
