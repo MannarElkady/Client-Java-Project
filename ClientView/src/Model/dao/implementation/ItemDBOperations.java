9c6f908211d524591920980500bd9ce1cc466bec
ahmedIbrahemis <ahmed.ibrahem@fci.helwan.edu.eg>
2020-01-10 10:21:14 +0200

    add GitItemCollaborators Functionality

--- ClientView/src/Model/dao/implementation/ItemDBOperations.java
+++ ClientView/src/Model/dao/implementation/ItemDBOperations.java
@@ -6,11 +6,22 @@
 package Model.dao.implementation;
 
 import Model.GsonParser;
+import Model.MainFormHandler;
 import Model.RequestEntity;
 import Model.entities.ItemEntity;
 import java.util.ArrayList;
 import Model.SocketConnection;
 import Model.entities.AssignFriendTodoEntity;
+import Model.entities.TodoEntity;
+import Model.entities.UserEntity;
+import clientview.ClientView;
+import clientview.TodoFormXMLController;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 /**
  *
@@ -81,5 +92,33 @@ public class ItemDBOperations {
         } else {
             System.out.println("Item not deleted successfully");
         }
+    }
+    
+     public static void getItemCollaborators(ItemEntity itemToUpdate) {
+        ArrayList<ItemEntity> list = new ArrayList<>();
+        list.add(itemToUpdate);
+        RequestEntity<ItemEntity> updateRequest = new RequestEntity("ItemDBOperations", "getItemCollaborators", list);
+        SocketConnection.getInstance().getPrintStreamInstance().println(GsonParser.parseToJson(updateRequest));
+    }
+           public void getItemCollaboratorsResonse(ArrayList<UserEntity> collabotarors) {
+           //clear
+         // TodoFormXMLController.clearTest();
+        if (collabotarors == null || collabotarors.size() == 0) {
+            System.out.println("no user in this item");
+        } else {
+            System.out.println("c"+collabotarors.size());
+            for(int i=0;i<collabotarors.size();i++){
+                System.out.println("ddd"+collabotarors.get(i).getUsername());
+            }
+            //TodoFormXMLController.setCollaboratorList(collabotarors);
+        }
+//        Parent root;
+//        try {
+//            root = FXMLLoader.load(getClass().getResource("/clientview/TodoFormXML.fxml"));
+//            Scene scene = ClientView.mainStage.getScene();
+//            scene.setRoot(root);
+//        } catch (IOException ex) {
+//            Logger.getLogger(MainFormHandler.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 }
