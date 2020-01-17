c08bc98132406d894735ec8587c7e32102a55b07
HimaAbousalem <hima.abousalem@gmail.com>
2020-01-17 17:09:01 +0200

    finish item tasks

--- ClientView/src/clientview/ClientView.java
+++ ClientView/src/clientview/ClientView.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clientview;
 
 import Model.SocketConnection;
-import Model.dao.implementation.ComponentDBOperations;
 import Model.dao.implementation.UserDBOperations;
-import Model.entities.ComponentEntity;
 import Model.entities.UserEntity;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,10 +10,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import java.io.IOException;
-
-import java.util.ArrayList;
-
-
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Platform;
@@ -49,23 +38,12 @@ public class ClientView extends Application {
             try {
                 UserDBOperations.logout(ClientView.currentUser);
                 SocketConnection.getInstance().closeSocketConnection();
-                UserDBOperations.logout(ClientView.currentUser);   
             } catch (IOException ex) {
                 Logger.getLogger(ClientView.class.getName()).log(Level.SEVERE, null, ex);
             }
             Platform.exit();
             System.exit(0);
         });
-        SocketConnection.getInstance();
-        ArrayList<Integer> users = new ArrayList<>();        
-        users.add(1);
-       
-        //NotificationDBOperations.receiveNotifications(users);
-                
-/* TodoEntity todo = new TodoEntity(2, "jljlk", "#1212", 1, "Done", "New description", Date.valueOf("2020-01-01"), Date.valueOf("2020-12-01"));
-        TodoListDBOperations.deleteTodo(todo);*/
-        ComponentEntity co=new ComponentEntity(3, 9,"","",0);
-        ComponentDBOperations.deleteComponent(co);
     }
 
     /**
